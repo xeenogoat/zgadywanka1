0f0d46f57ce1f11cc64295876cec9a6accd0492e
xeenogoat <07kamilos@gmail.com>
2024-09-04 15:23:55 +0200

    pusty projekt

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    //\Nazwa pliku i nazwa klasy muszą być takie same
+    //nazwwy klas powiny być w z wielkiej litery - tak karze obyczaj
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
