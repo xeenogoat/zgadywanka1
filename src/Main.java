baabfd19117b0679ba840e8519e8e668dae959b2
xeenogoat <07kamilos@gmail.com>
2024-09-05 12:41:48 +0200

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
-    //\Nazwa pliku i nazwa klasy muszą być takie same
-    //nazwwy klas powiny być w z wielkiej litery - tak karze obyczaj
+    //Nazwa pliku i nazwa klasy muszą być takie same
+    //nazwy klas powinny być z wielkiej litery - tak karze obyczaj
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
-        System.out.println("Hello world");
-        System.out.println("witamy na lekcjach prograsmowania");
-    int wylosowanaLiczba = (int)(Math.random()*10+1);//rzutowanie
+        System.out.println("Witamy na lekcjach programowania");
+        //typ prosty - tylko wartośc, z małej litery
+        /*
+        boolean,
+        int long  short byte
+        float double
+        char
+         */
+        //typ zlozony - obiekt ktory ma walansosci i metody, z wielkiej litery
+        int wylosowanaLiczba = (int)(Math.random()*10+1);//rzutowanie
+
         System.out.println(wylosowanaLiczba);
-    }}
-//losowanie liczby
-//typ prosty - tylko wartosc, z malej litery
-//typ zlozony - obiekt ktory ma wlasnosci i metody, z duzej litery
+
+        System.out.println("Zgadnij liczbę");
+        Scanner klawiatura = new Scanner(System.in);
+        int wpisanaLiczba = klawiatura.nextInt();
+
+        if(wpisanaLiczba == wylosowanaLiczba){
+            System.out.println("Gratulacje wygrana");
+        }
+        else {
+            System.out.println("Pudło");
+        }
+
+        int roznica = wylosowanaLiczba>wpisanaLiczba? wylosowanaLiczba-wpisanaLiczba : wpisanaLiczba-wylosowanaLiczba;
+
+    }
+}
 
